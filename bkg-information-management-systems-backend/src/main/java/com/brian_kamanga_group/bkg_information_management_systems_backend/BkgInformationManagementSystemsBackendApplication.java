7cc98b687b73776ed6ec4f221a28d7a8bcfc06f1
briankamanga <73723985+briankamanga@users.noreply.github.com>
2026-01-07 21:03:15 +0000

    Add files via upload

---
+++ bkg-information-management-systems-backend/src/main/java/com/brian_kamanga_group/bkg_information_management_systems_backend/BkgInformationManagementSystemsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brian_kamanga_group.bkg_information_management_systems_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkgInformationManagementSystemsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkgInformationManagementSystemsBackendApplication.class, args);
+	}
+
+}
---
+++ bkg-information-management-systems-backend/src/test/java/com/brian_kamanga_group/bkg_information_management_systems_backend/BkgInformationManagementSystemsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian_kamanga_group.bkg_information_management_systems_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkgInformationManagementSystemsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
